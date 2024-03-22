0d41b769eafea933b8d0024a898d87883c091cbb
Anshul Thakur <132456667+anshulthakur1010@users.noreply.github.com>
2024-03-22 19:05:24 +0530

    Delete Redux/Counter-React-Redux-2 directory

--- Redux/Counter-React-Redux-2/src/components/DisplayCounter.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-
-const DisplayCounter = () => {
-  const { counterVal } = useSelector((store) => store.counter);
-
-  return <p className="lead mb-4">Counter Current Value:{counterVal}</p>;
-};
-
-export default DisplayCounter;
